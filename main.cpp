9a4f7548c59fa127b2d434e138011c91f31c14d9
hihikomori <ghhhhhnika@gmail.com>
2026-02-27 14:37:06 +0300

    declaration of simplified interface

--- main.cpp
+++ main.cpp
@@ -6,5 +6,25 @@ template < class T > struct BiList
   BiList< T > *prev;
 };
 
+template < class T > BiList< T > *add(BiList< T > *h, const T &d);
+
+template < class T > BiList< T > *insert(BiList< T > *h, const T &d);
+
+template < class T > BiList< T > *add(BiList< T > *h, const T &d);
+
+template < class T > BiList< T > *insert(BiList< T > *h, const T &d);
+
+template < class T > BiList< T > *cut(BiList< T > *h);
+
+template < class T > BiList< T > *erase(BiList< T > *h);
+
+template < class T > BiList< T > *clear(BiList< T > *h);
+
+template < class T > BiList< T > *clear(BiList< T > *h, BiList< T > *e);
+
+template < class T, class F > F traverse_from_head(F f, BiList< T > *h, BiList< T > *e);
+
+template < class T, class F > F traverse_from_tail(F f, BiList< T > *h, BiList< T > *e);
+
 int main()
 {}
